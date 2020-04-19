0445c8b0d179ae35b1a78f8fb9c65cba5298ff0a
laibasagheer <54498085+laibasagheer@users.noreply.github.com>
2020-04-19 04:52:39 -0700

    Add files via upload

---
+++ Untitled28.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+n= int(input('how many terms '))
+a = 0 
+b = 1 
+print (a)
+print (b)
+for c  in range (2,n):
+         c = a+b
+         a=b
+         b=c
+         print(c)
+            
+    
+        
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
